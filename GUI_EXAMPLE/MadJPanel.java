4ae924d5db05741aec9ac6d694bb58e6070b7765
Adem Sinankili <adem.sinankili@siemens.com>
2022-05-04 11:14:48 +0200

    Add provisional gui

---
+++ GUI_EXAMPLE/MadJPanel.java
@@ -0,0 +1,24 @@
+import javax.swing.JPanel;
+import java.awt.Color;
+
+public class MadJPanel extends JPanel 
+{
+    public MadJPanel(Color color)
+    {
+        super();
+        this.setBackground(color);
+    }
+
+    public MadJPanel(boolean visibility)
+    {
+        super();
+        this.setVisible(visibility);
+    }
+
+    public MadJPanel(boolean visibility, Color color)
+    {
+        super();
+        this.setVisible(visibility);
+        this.setBackground(color);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MDA - SPE Project
+## Project
+
+In here the is an implementation of the well know game "Mensch aergere dich nicht".
+This is the group repo of Gent, Jannik and Adem for the Siemens Professional Education class.
